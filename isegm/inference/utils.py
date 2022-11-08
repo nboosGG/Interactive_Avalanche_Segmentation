ff1b5f73ebc73cea21c8ae95f958b1abb4bb79af
Lucien <oberson@SLFL27501>
2022-11-08 11:40:15 +0100

    First Try of Training with Avalanche Data

--- isegm/inference/utils.py
+++ isegm/inference/utils.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import torch
 import numpy as np
 
-from isegm.data.datasets import GrabCutDataset, BerkeleyDataset, DavisDataset, SBDEvaluationDataset, PascalVocDataset
+from isegm.data.datasets import GrabCutDataset, BerkeleyDataset, DavisDataset, SBDEvaluationDataset, PascalVocDataset, LawineDataset
 from isegm.utils.serialization import load_model
 
 
@@ -60,6 +60,8 @@ def get_dataset(dataset_name, cfg):
         dataset = PascalVocDataset(cfg.PASCALVOC_PATH, split='test')
     elif dataset_name == 'COCO_MVal':
         dataset = DavisDataset(cfg.COCO_MVAL_PATH)
+    elif dataset_name == 'Lawine':
+        dataset = LawineDataset(cfg.LAWINE_PATH)
     else:
         dataset = None
 
